d2c97c59dc7d3a1052b970fbc55a8039cf38b367
Aspics <aspic.connect@gmail.com>
2026-03-20 02:12:28 +0000

    fixed debug_toolbar still in urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+"""
 from django.contrib import admin
 from django.urls import include, path
 from debug_toolbar.toolbar import debug_toolbar_urls
@@ -22,3 +24,17 @@ urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]  + debug_toolbar_urls()
+"""
+
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+
+urlpatterns = [
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
